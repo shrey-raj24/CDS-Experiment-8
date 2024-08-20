674e4ce7bc2a9be60c2373bdbf0eff739934154a
Shrey raj <shreyraj37@gmail.com>
2024-08-21 02:29:05 +0530

    Add files via upload

---
+++ exp8(d).cpp
@@ -0,0 +1,36 @@
+// NAME - SHREY RAJ
+// PRN - 23070123123
+// EXPERIMENT - 8(D)  
+
+// Program to get the sum of the diagonal elements. 
+
+#include <iostream>
+using namespace std;
+
+
+const int MAX = 100;
+
+void printDiagonalSums(int mat[][MAX], int n) 
+{ 
+    int principal = 0;
+    
+    for (int i = 0; i < n; i++)  
+    { 
+        // Condition for principal diagonal 
+        principal += mat[i][i]; 
+    } 
+  
+    cout << "Sum of the diagonal elements is: " << principal << endl; 
+} 
+
+int main() 
+{ 
+    int a[][MAX] = {{1, 2, 3, 4},  
+                    {8, 7, 6, 5},  
+                    {2, 1, 4, 3},  
+                    {5, 6, 7, 8}}; 
+    printDiagonalSums(a, 4); 
+    return 0; 
+}
+
+// Sum of the diagonal elements is: 20 
